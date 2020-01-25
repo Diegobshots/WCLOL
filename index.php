9b5f8f8daa619aca96c92523e8a9b4f8537c8242
diegobshots <blancolopez4@gmail.com>
2020-01-25 16:43:31 +0100

    modificaciones Diego prueba

--- index.php
+++ index.php
@@ -14,24 +14,30 @@ if(!isset($_REQUEST['nombreInvocador']) || $_REQUEST['nombreInvocador'] == "" ){
     include("formulario.php");
 }else{
 
+    //Transformamos el nombre de invocador a formato para URL espacios -> %20%
+
     $nombre = str_replace(" ","%20",$_REQUEST['nombreInvocador']);
     $server = $_REQUEST['server'];
 
   
+    //Obtenemos el json que nos devuelve la api pasandole el nombre de invocador y servidor
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://$server.api.riotgames.com/lol/summoner/v4/summoners/by-name/$nombre?api_key=RGAPI-3d96a6a4-36cf-4bfc-b552-615b8a83a6be");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $res = curl_exec($ch);
     curl_close($ch);
 
+    //Transformamos el archivo json a objeto PHP para manejar los datos
     $res = json_decode($res);
 
+    //Validamos errores de conexion o falta de info por parte del servidor
     if(isset($res->status)){
         if($res->status->status_code == "404"){
             echo "<h1>No existe ese man<h1>";
         }
     }else{
         
+        //hacemos una segunda consulta a la api, extrayendo de la primera el ID de invocador
         $summonerID = $res->id;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/".$summonerID."?api_key=RGAPI-3d96a6a4-36cf-4bfc-b552-615b8a83a6be");
@@ -53,6 +59,8 @@ if(!isset($_REQUEST['nombreInvocador']) || $_REQUEST['nombreInvocador'] == "" ){
             $fecha = date('m/d/Y H:i:s', $partida);
         
             echo "<h1>$fecha</h1>";
+            //Hay que pillar la hora del sistema y restarle la hora a la que empezó la partida para pillar los segundos que lleva iniciada
+        
         }
 
 
